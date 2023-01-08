32b6e809a691eb54cfffc19fa5a03b7be2525149
Adam Garcia <adgar1523@gmail.com>
2023-01-07 20:13:29 -0700

    travel place delete request

--- index.js
+++ index.js
@@ -1,37 +1,70 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const methodOverride = require('method-override');
+const { v4: uuid } = require('uuid');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.set('views', path.join(__dirname, 'views'));
 app.use('/public', express.static('public'));
 app.set('view engine', 'ejs');
+app.use(methodOverride('_method'));
 
 const user = {
-    name: "Adam",
+    name: "Adam Garcia",
     age: 26,
     occupation: "Web Developer",
-    reasonForTravel: "See the world",
+    reasonForTravel: "n",
     places: [
         {
-            place: "Barcelona, Spain",
-            stay: "Spanish Hotel",
-            leaveDate: new Date('02/10/2023'),
-            returnDate: new Date('03/05/2023')
+            country: "Spain",
+            city: "Barcelona",
+            img: "public/images/barcelona_spain.jpg",
+            id: uuid()
         },
         {
-            place: "Cairo, Egypt",
-            stay: "Random Hotel",
-            leaveDate: new Date('05/10/2023'),
-            returnDate: new Date('06/05/2023')
+            country: "Egypt",
+            city: "Cairo",
+            img: "public/images/cairo_egypt.jpeg",
+            id: uuid()
         }
     ]
 }
 
-app.get('/', (req, res) => {
-    res.render('dashboard', { user });
+let userPlaces = user.places;
+
+app.get('/places', (req, res) => {
+    res.render('dashboard', { user, userPlaces });
 });
+
+app.get('/places/new', (req, res) => {
+    res.render('new');
+});
+
+app.get('/places/:id', (req, res) => {
+    const { id } = req.params;
+    const place = userPlaces.find(p => p.id == id);
+    res.render('place', { place });
+});
+
+app.post('/places', (req, res) => {
+    const { country, city, id } = req.body;
+    userPlaces.push({country, city, id: uuid()})
+    console.log(userPlaces);
+    res.redirect('/places');
+});
+
+app.delete('/places/:id', (req, res) => {
+    const { id } = req.params;
+    userPlaces = userPlaces.filter(p => p.id !== id);
+    console.log(userPlaces);
+    res.redirect('/places');
+})
+
+
+
+
 
 app.listen(3000, () => {
     console.log('listening on port 3000!');
