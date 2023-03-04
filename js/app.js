05965ee3dabbaf7629eadd0c7db4aa0d4ea6cfae
Adam Garcia <adgar1523@gmail.com>
2023-03-03 19:23:12 -0700

    redesign

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 const navbar = document.querySelector('.navbar');
 const navLinks = document.querySelectorAll('.navbar li');
 const toggleNav = document.querySelector('.hamburger');
+const toggleNavBars = document.querySelectorAll('.hamburger span');
 const mainBody = document.querySelector('body');
+const aboutSection = document.querySelector('#about');
 
 toggleNav.addEventListener('click', () => {
     navbar.classList.toggle('active');
@@ -16,3 +18,24 @@ navLinks.forEach((li) => {
         mainBody.classList.toggle('unfocus');
     });
 });
+
+var observer = new IntersectionObserver(function(entries) {
+    if (entries[0].isIntersecting) {
+        toggleNavBars.forEach(bar => {
+            bar.classList.toggle("scrolled");
+        })
+        observer.unobserve(aboutSection);
+    }
+});
+
+observer.observe(aboutSection);
+
+// window.onscroll = function() {
+//     toggleNavBars.forEach(bar => {
+//         if (window.pageYOffset > 600) {
+//           bar.classList.add('scrolled')
+//         } else {
+//           bar.classList.remove('scrolled')
+//         }
+//     })
+//   }
